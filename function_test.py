73a14ffc20f053f7f53fa92f7cb10ae67841c43c
Mr.ChePro <khanhlinhdangthditrach@gmail.com>
2024-12-11 12:39:02 +0300

    11.12.24

--- function_test.py
+++ function_test.py
@@ -1,76 +1,54 @@
-# Copyright (C) 2023 The Qt Company Ltd.
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
-from __future__ import annotations
+def get_all_nt(x):
+    if x < 1:
+        return
+    list_uoc = []
+    for i in range(1,x+1):
+        sochia = x%i 
+        if sochia == 0:
+            list_uoc.append(i)
+    return list_uoc
 
-"""PySide6 port of the location/mapviewer example from Qt v6.x"""
+def check_nt(x):
+    if get_all_nt(x)==[1,x]:
+        return True
+    return False
 
-import os
-import sys
-from pathlib import Path
-
-from PySide6.QtQml import QQmlApplicationEngine
-from PySide6.QtGui import QGuiApplication
-from PySide6.QtNetwork import QSslSocket
-from PySide6.QtCore import QCoreApplication, QMetaObject, Q_ARG
-
-HELP = """Usage:
-plugin.<parameter_name> <parameter_value> - Sets parameter = value for plugin"""
-
-
-def parseArgs(args):
-    parameters = {}
-    while args:
-        param = args[0]
-        args = args[1:]
-        if param.startswith("--plugin."):
-            param = param[9:]
-            if not args or args[0].startswith("--"):
-                parameters[param] = True
-            else:
-                value = args[0]
-                args = args[1:]
-                if value in ("true", "on", "enabled"):
-                    parameters[param] = True
-                elif value in ("false", "off", "disable"):
-                    parameters[param] = False
-                else:
-                    parameters[param] = value
-    return parameters
+nt = check_nt(3)
 
 
-if __name__ == "__main__":
-    additionalLibraryPaths = os.environ.get("QTLOCATION_EXTRA_LIBRARY_PATH")
-    if additionalLibraryPaths:
-        for p in additionalLibraryPaths.split(':'):
-            QCoreApplication.addLibraryPath(p)
+def get_min_nt(x,list_min_nt=[]):
+    if check_nt(x):
+        list_min_nt.append(x)
+        return list_min_nt
+    lst = get_all_nt(x)
+    print(lst)
+    minnt = None
+    for i in lst:
+        if i!=1 and i!=x:
+            print(i)
+            if check_nt(i):
+                list_min_nt.append(i)
+                minnt = i
+                break
+    if minnt:
+        nt = int(x/minnt)
+        print(nt,type(nt))
+        get_min_nt(nt,list_min_nt)
 
-    application = QGuiApplication(sys.argv)
-    name = "QtLocation Mapviewer example"
-    QCoreApplication.setApplicationName(name)
 
-    args = sys.argv[1:]
-    if "--help" in args:
-        print(f"{name}\n\n{HELP}")
-        sys.exit(0)
+def total(lst):
+    return sum(lst)
 
-    parameters = parseArgs(args)
-    if not parameters.get("osm.useragent"):
-        parameters["osm.useragent"] = name
+def get_rs(x):
+    list_min_nt = []
+    get_min_nt(x,list_min_nt)
+    print(list_min_nt)
+    if len(list_min_nt) == 1:
+        return list_min_nt
+    x = total(list_min_nt)
+    get_rs(x)
 
-    engine = QQmlApplicationEngine()
-    engine.rootContext().setContextProperty("supportsSsl",
-                                            QSslSocket.supportsSsl())
-    engine.addImportPath(Path(__file__).parent)
-    engine.loadFromModule("MapViewer", "Main")
-    engine.quit.connect(QCoreApplication.quit)
+while True:
+    kq = get_rs(int(input()))
+    print(kq)
 
-    items = engine.rootObjects()
-    if not items:
-        sys.exit(-1)
-
-    QMetaObject.invokeMethod(items[0], "initializeProviders",
-                             Q_ARG("QVariant", parameters))
-
-    exit_code = application.exec()
-    del engine
-    sys.exit(exit_code)
