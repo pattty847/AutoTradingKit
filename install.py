e81a7b961c3da3b19c737042362e3d7d08524f5b
Pham Cong Che <khanhlinhdangthditrach@gmail.com>
2025-03-11 05:07:40 +0700

    from mac

--- install.py
+++ install.py
@@ -1,1 +1,3 @@
-nuitka --standalone --onefile --enable-plugin=pyside6 --output-dir=build --remove-output --follow-imports --nofollow-import-to=tkinter --windows-console-mode=disable --windows-icon-from-ico=atklip/appdata/appico.ico ATK.py
+nuitka --standalone --onefile --macos-create-app-bundle --enable-plugin=pyside6 --output-dir=build --remove-output --follow-imports --nofollow-import-to=tkinter --windows-console-mode=disable --windows-icon-from-ico=atklip/appdata/appico.ico ATK.py
+
+FATAL: options-nanny: Error, package 'Foundation' requires '--macos-create-app-bundle' to be used or else it cannot work.
